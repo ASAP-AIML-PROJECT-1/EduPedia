a93315a23f1368dc700a930ef74cce4d08034b49
ILLIYASK <Illiyask26061999@gmail.com>
2023-02-14 13:50:25 +0530

    login stud,teac,comp style modified

--- main/EduPedia.py
+++ main/EduPedia.py
@@ -12,7 +12,7 @@ window.config(bg="black")
 
 def std():
     frame_login.place_forget()
-    frame_stud_log.place(x=200,y=100)
+    frame_stud_log.place(x=350,y=200)
 
 
 def showpsd():
@@ -43,30 +43,29 @@ button_comp.place(x=250,y=175)
 
 # student login
 
+# student login
+
 frame_stud_log=Frame(window,bg="yellow")
-label_usr=Label(frame_stud_log,text="Enter Username",)
-entry_usr=Entry(frame_stud_log)
-label_pwd=Label(frame_stud_log,text="Enter Password")
-entry_pwd=Entry(frame_stud_log,show="*")
+label_usr=Label(frame_stud_log,text="Enter Username ",bg="yellow",font=("Helvetica", "16"))
+entry_usr=Entry(frame_stud_log,font=("Helvetica", "16"))
+label_pwd=Label(frame_stud_log,text="Enter Password ",bg='yellow',font=("Helvetica", "16"))
+entry_pwd=Entry(frame_stud_log,show="*",font=("Helvetica", "16"))
 v=IntVar(value=0)
-check_pwd=Checkbutton(frame_stud_log,variable=v,onvalue=1,offvalue=0,command=lambda :showpsd())
-button_submit=Button(frame_stud_log,text="Submit")
-button_forget=Button(frame_stud_log,text="forgot password")
-button_create=Button(frame_stud_log,text="Create account")
+check_pwd=Checkbutton(frame_stud_log,text="show password",variable=v,onvalue=1,offvalue=0,command=lambda :showpsd())
+button_submit=Button(frame_stud_log,text="Submit",height=2,width=15,bg="green")
+button_forget=Button(frame_stud_log,text="forgot password",bg="blue",fg="white")
+button_create=Button(frame_stud_log,text="Create account",fg="blue")
 button_back=Button(frame_stud_log,text="back",command=lambda :home())
 
-label_usr.grid(row=0,column=0)
+label_usr.grid(row=0,column=0,padx=20,pady=20)
 entry_usr.grid(row=0,column=1)
 label_pwd.grid(row=1,column=0)
 entry_pwd.grid(row=1,column=1)
-check_pwd.grid(row=1,column=2)
-button_submit.grid(row=2,column=1)
-button_forget.grid(row=3,column=1)
-button_create.grid(row=4,column=1)
+check_pwd.grid(row=1,column=2,padx=10)
+button_submit.grid(row=2,column=1,pady=10)
+button_forget.grid(row=2,column=2)
+button_create.grid(row=3,column=2)
 button_back.grid(row=5,column=1)
-
-
-
 
 
 

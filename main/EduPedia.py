acaa64626780c206d5e81960265ec71800784ccd
ansilmbabl <86063895+ansilmbabl@users.noreply.github.com>
2023-02-18 22:39:41 +0530

    Update EduPedia.py
    updated create blog function
    favorite function created

--- main/EduPedia.py
+++ main/EduPedia.py
@@ -224,6 +224,41 @@ def profile(frame,username,user):
         button_submit.grid(row=8, column=1, pady=20)
         button_cancel.grid(row=8, column=0, pady=20)
 
+    def create_blog(frame,username,user):
+
+
+        create_blog_frame=Frame(frame,width=800,height=550,bg="black")
+
+        author_name_label = Label(create_blog_frame, text="author_name", font=("Helvetica", "16"), fg='white', bg="black",anchor="nw",width=15)
+        author_name_entry = Entry(create_blog_frame, font=("Helvetica", "16"),width=50)
+        category_label = Label(create_blog_frame, text="category", font=("Helvetica", "16"), fg='white', bg="black",anchor="nw",width=15)
+        category_entry = Entry(create_blog_frame, font=("Helvetica", "16"),width=50)
+        blog_name_label = Label(create_blog_frame, text="blog_name", font=("Helvetica", "16"), fg='white',
+                                  bg="black",anchor="nw",width=15)
+        blog_name_entry = Entry(create_blog_frame, font=("Helvetica", "16"),width=50)
+        blog_label = Label(create_blog_frame, text="blog", font=("Helvetica", "16"), fg='white', bg="black",anchor="nw",width=15)
+        blog_text = Text(create_blog_frame, font=("Helvetica", "16"),width=50,height=5)
+        tags_label = Label(create_blog_frame, text="tags", font=("Helvetica", "16"), fg='white',
+                                bg="black",anchor="nw",width=15)
+        tags_entry = Entry(create_blog_frame, font=("Helvetica", "16"),width=50)
+        close_button = Button(create_blog_frame, text="X",bg="red", width=2,command=lambda: close(create_blog_frame))
+        create_button=Button(create_blog_frame,text="Create",bg="green")
+
+
+        create_blog_frame.place(x=300,y=150)
+
+        close_button.grid(row=0,column=2,sticky='w',pady=5,padx=10)
+        author_name_label.grid(row=2,column=0)
+        author_name_entry.grid(row=2,column=1)
+        category_label.grid(row=3,column=0,pady=20)
+        category_entry.grid(row=3,column=1)
+        blog_name_label.grid(row=4,column=0)
+        blog_name_entry.grid(row=4,column=1)
+        blog_label.grid(row=5,column=0,pady=20)
+        blog_text.grid(row=5,column=1,pady=20)
+        tags_label.grid(row=6,column=0)
+        tags_entry.grid(row=6,column=1)
+        create_button.grid(row=8,column=1,pady=20)
 
     def view_profile(frame,username,user):
         if user == "student":
@@ -490,7 +525,7 @@ def profile(frame,username,user):
     # top_icons
     search_button = Button(top_icons, text="search", bg='white', foreground='blue', font=("Helvetica", "14"), width=13, command=lambda: search_window())
     vlog_button = Button(top_icons, text="vlogs", bg='white', foreground='blue', font=("Helvetica", "14"), height=1, width=13, command = lambda: blog_feed(user))
-    create_vlog_button = Button(top_icons, text="create vlogs", bg='white', foreground='blue', font=("Helvetica", "14"), height=1, width=13)
+    create_vlog_button = Button(top_icons, text="create vlogs", bg='white', foreground='blue', font=("Helvetica", "14"), height=1, width=13,command=lambda :create_blog(frame,username,user))
     index_button = Button(top_icons, text="index", bg='white', foreground='blue', font=("Helvetica", "14"), height=1, width=13,command=lambda : index())
     extra_button = Button(top_icons, text="Extra", bg='white', foreground='blue', font=("Helvetica", "14"), height=1, width=13)
 
